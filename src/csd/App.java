b3a130987503e91d6b84a12095df5456d605af48
hyenicil <57847728+hyenicil@users.noreply.github.com>
2024-07-13 23:20:48 +0300

    Example 3 tamamlandi.

--- src/csd/App.java
+++ src/csd/App.java
@@ -2,10 +2,12 @@ package csd;
 
 import csd.examples.Example1;
 import csd.examples.Example2;
+import csd.examples.Example3;
 
 public class App {
     public static void main(String[] args) {
         //Example1.run();
-        Example2.run();
+        //Example2.run();
+        Example3.run();
     }
 }
