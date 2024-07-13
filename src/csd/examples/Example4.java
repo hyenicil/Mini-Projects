442ded4c4daa2f514dfc14dccdb8a02f79c1278e
hyenicil <57847728+hyenicil@users.noreply.github.com>
2024-07-13 23:34:37 +0300

    Example 4 tamamlandi

---
+++ src/csd/examples/Example4.java
@@ -0,0 +1,35 @@
+package csd.examples;
+
+public class Example4 {
+
+    public static void run() {
+
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        while (true) {
+
+            System.out.print("Tohum değeri giriniz: ");
+            long seed = sc.nextLong();
+
+            java.util.Random random = new java.util.Random(seed);
+
+            for(int i =0; i<10; i++)
+                System.out.printf("%02d", random.nextInt(100));
+
+            System.out.println();
+
+            random.setSeed(System.nanoTime());
+
+            for (int i =0; i<10; i++)
+                System.out.printf("%02d", random.nextInt(100));
+
+            System.out.println();
+
+            if(seed == 0)
+                break;
+        }
+    }
+}
+//Tohum değeri giriniz: 12
+//66125633241150159621
+//27408437102883540322
